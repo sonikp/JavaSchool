34e1b2ca157d6e70ab07073dd03ebe156d2b3fd3
sonikp <mfloerchinger@yahoo.com>
2017-01-22 13:56:01 -0800

    update

---
+++ JavaII/JavaII-wk08-Assignment/JavaII_wk08_Assignment.java
@@ -0,0 +1,7 @@
+public class JavaII_wk08_Assignment
+{
+	public static void main(String[] args)
+	{
+		System.out.println("JavaII_wk08_Assignment");
+	}
+}
---
+++ JavaII/JavaII-wk09-Assignment/JavaII_wk09_Assignment.java
@@ -0,0 +1,7 @@
+public class JavaII_wk09_Assignment
+{
+	public static void main(String[] args)
+	{
+		System.out.println("JavaII_wk09_Assignment");
+	}
+}
