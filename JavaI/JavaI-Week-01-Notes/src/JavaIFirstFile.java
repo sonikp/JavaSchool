b9957958a2a6e334ff3f9475bbb61f68e06abafc
sonikp <mfloerchinger@yahoo.com>
2017-01-22 12:09:47 -0800

    initial

--- JavaI/JavaI-Week-01-Notes/src/JavaIFirstFile.java
+++
@@ -1,7 +0,0 @@
-public class JavaIFirstFile
-{
-	public static void main(String[] args)
-	{
-		System.out.println("This is the first file in the JavaI-Week-01 folder");
-	}
-}
--- JavaII/JavaII-Week-01-Notes/src/JavaIIWeek01NotesFirst.java
+++
@@ -1,7 +0,0 @@
-public class JavaIIWeek01NotesFirst
-{
-	public static void main(String[] args)
-	{
-		System.out.println("This is the first file in the JavaI-Week-01 folder");
-	}
-}
