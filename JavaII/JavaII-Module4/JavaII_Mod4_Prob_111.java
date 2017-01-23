83d4f35dbe9ea1ecac0c781a3430e0bd78c482dd
sonikp <mfloerchinger@yahoo.com>
2017-01-23 08:27:12 -0800

    update

--- JavaII/JavaII-Module4/JavaII_Mod4_Prob_111.java
+++ JavaII/JavaII-Module4/JavaII_Mod4_Prob_111.java
@@ -2,7 +2,41 @@ public class JavaII_Mod4_Prob_111
 {
 	/*
 	 what is a field? 
-	 A field 
+	 A field is the data element that represents a value for an object. This is defined 
+	 as data type and name.
+	 
+	  What is a constructor?
+	  A constructor is an initialization method to initialize objects or fields. If the
+	  compiler doesn't see any constructors, it will automatically create one that does
+	  not take any parameters, and doesn't change the value of the fields. Constructors
+	  are identified in that they do not declare a return type.
+	  
+	  What does override mean?
+	  Creating a method with the same method signature (method name / parameter list) as
+	  the parent method will result in that method being called instead of the method 
+	  defined in the parent class.
+	  
+	  Specifying the parent class of the method you are declaring?
+	  Done with the use of "extends" after the local class name and the method or the 
+	  declared parent. e.g public class lesson extends student.
+	  
+	  Is there a parent class?
+	  The default parent class when none has been declared is always java.lang.Object
+	  To get class: objectName.getClass(); aka subclass or derived class
+	  To get parent: objectName.getSuperClass(); aka superclass or baseclass
+	  
+	  How do you overload a constructor?
+	  Overloading constructors, is declaring constructors with different parameter
+	  lists for all the potential different options on the class.
+	  
+	  What is the difference between overload and override?
+	  Overload is locally creating the same constructor or method name with different 
+	  data elements?
+	  Overriding creating a local version of a parent method with the same method name
+	  and parameter list.
+	  
+	  How do you create an array?
+	  type[] name = {e1,e2,e3,e4};
 	 
 	 
 	 
